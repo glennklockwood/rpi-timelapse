2769b3a58d83058aa8020fc14dc0d31f307ef315
Glenn K. Lockwood <glennklockwood@gmail.com>
2020-07-25 15:10:35 -0700

    remove hard-coded output filename

--- make_video.sh
+++ make_video.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+    output_file="timelapse.mp4"
+else
+    output_file="$1"
+fi
 j=0
 for i in capture-*.jpg
 do
@@ -7,6 +12,6 @@ do
     let j++
 done
 
-avconv -r 10 -i frame%04d.jpg -r 10 -vcodec libx264 timelapse.mp4
+avconv -r 10 -i frame%04d.jpg -r 10 -vcodec libx264 "$output_file"
 
 rm frame*.jpg
